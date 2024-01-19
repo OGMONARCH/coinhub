c26899f62b78f3ac34b0093a2b06f0c825416c6a
OGMONARCH <149230226+OGMONARCH@users.noreply.github.com>
2024-01-19 08:51:17 +0100

    😒

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     const [coin, setCoin] = useState([])
   
   const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
-  //  const URL = "https://api.coingecko.com/api/v3/coins/list"
+
     useEffect(() => {
       axios.get(URL)
       .then((response)=>{
